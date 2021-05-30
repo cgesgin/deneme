335677950e0fe86c3c6500a1dfb462fa2315b733
cgesgin <cevher-5@hotmail.com>
2021-05-30 17:33:21 +0300

    Actor icin sayfalama işlemi yapıldı

--- src/java/dao/ActorDAO.java
+++ src/java/dao/ActorDAO.java
@@ -1,4 +1,3 @@
- 
 package dao;
 
 import entity.Actor;
@@ -8,25 +7,25 @@ import java.util.ArrayList;
 import java.util.List;
 import util.DBConnecter;
 
- 
 public class ActorDAO extends DBConnecter {
 
     public void create(Actor actor) {
         try {
             Statement st = this.connect().createStatement();
-            st.executeUpdate("insert into actor (first_name,last_name) values ('" + actor.getFirstName()+ "','"+actor.getLastName()+"')");
+            st.executeUpdate("insert into actor (first_name,last_name) values ('" + actor.getFirstName() + "','" + actor.getLastName() + "')");
         } catch (Exception ex) {
             System.out.println("create" + ex.getMessage());;
         }
     }
 
-    public List<Actor> read() {
+    public List<Actor> read(int page, int pageSize) {
+        int start = (page - 1) * pageSize;
         List<Actor> list = new ArrayList<Actor>();
         try {
             Statement st = this.connect().createStatement();
-            ResultSet rs = st.executeQuery("select * from actor order by actor_id asc");
+            ResultSet rs = st.executeQuery("select * from actor order by actor_id asc limit " + pageSize + " offset " + start);
             while (rs.next()) {
-                Actor actor = new Actor(rs.getInt("actor_id"),rs.getString("first_name"), rs.getString("last_name"), rs.getDate("last_update"));
+                Actor actor = new Actor(rs.getInt("actor_id"), rs.getString("first_name"), rs.getString("last_name"), rs.getDate("last_update"));
                 list.add(actor);
             }
         } catch (Exception ex) {
@@ -35,10 +34,23 @@ public class ActorDAO extends DBConnecter {
         return list;
     }
 
+    public int count() {
+        int count = 0;
+        try {
+            Statement st = this.connect().createStatement();
+            ResultSet rs = st.executeQuery("select count(*) as c from actor ");
+            rs.next();
+            count = rs.getInt("c");
+        } catch (Exception ex) {
+            System.out.println("count() method " + ex.getMessage());;
+        }
+        return count;
+    }
+
     public void update(Actor actor) {
         try {
             Statement st = this.connect().createStatement();
-            st.executeUpdate("update actor set first_name='" + actor.getFirstName() + "',last_name='"+actor.getLastName()+"' where actor_id=" + actor.getActorId());
+            st.executeUpdate("update actor set first_name='" + actor.getFirstName() + "',last_name='" + actor.getLastName() + "' where actor_id=" + actor.getActorId());
         } catch (Exception ex) {
             System.out.println("" + ex.getMessage());;
         }
